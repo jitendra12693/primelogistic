c2518209a532ab87e9b2429b42049336fb592dec
Jitendra Dubey <jitendra.dubey@stits.co>
2022-12-25 22:32:03 +0530

    Index html changes and add new network data

--- InTimeCourier/Controllers/NetworkController.cs
+++ InTimeCourier/Controllers/NetworkController.cs
@@ -1,22 +1,43 @@
-﻿using System;
+﻿using InTimeCourier.App_Start;
+using InTimeCourier.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace InTimeCourier.Controllers
 {
+    [SessionAuthorize]
     public class NetworkController : Controller
     {
+        InLogisticModel db = new InLogisticModel();
         // GET: Network
         public ActionResult Index()
         {
-            return View();
+            var data = db.NetworkMaster.OrderBy(x => x.NetworkId).ToList();
+            return View(data);
         }
 
         public ActionResult AddNetwork(int? id)
         {
+            if(id!=null && id>0)
+            {
+                return View(db.NetworkMaster.Where(x=>x.NetworkId==id).FirstOrDefault());
+            }
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddNetwork(NetworkMaster network)
+        {
+            var response = db.Database.SqlQuery<Responsedetails>("exec uspNetworkInsertUpdate @NetworkId,@NetworkName,@Address,@Description,@UserId",
+                new SqlParameter("@NetworkId", network.NetworkId),
+                new SqlParameter("@NetworkName", network.NetworkName),
+                new SqlParameter("@Address", network.Address),
+                new SqlParameter("@Description", network.Description),
+                new SqlParameter("@UserId", int.Parse("0" + Session["UserId"]))).FirstOrDefault();
+            return RedirectToAction("Index");
         }
     }
 }
