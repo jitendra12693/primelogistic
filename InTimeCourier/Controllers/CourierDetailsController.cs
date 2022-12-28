670841c611e3583f6e8387c01202b0cf0b762bc3
Jitendra Dubey <jitendra.dubey@stits.co>
2022-12-28 22:46:54 +0530

    Export Excel Code implementation

--- InTimeCourier/Controllers/CourierDetailsController.cs
+++ InTimeCourier/Controllers/CourierDetailsController.cs
@@ -370,38 +370,18 @@ namespace InTimeCourier.Controllers
         //    List<CorrierDetails> list = GetCourierList(ref count, partyId, trackingNo, fromDate.Replace("/", ""), toDate.Replace("/", ""));
         //    return Json(list, JsonRequestBehavior.AllowGet);
         //}
-
-        static DataTable ConvertToDataTable<T>(List<T> models)
+        public ActionResult ResultExportToExcel()
         {
-            // creating a data table instance and typed it as our incoming model   
-            // as I make it generic, if you want, you can make it the model typed you want.  
-            DataTable dataTable = new DataTable(typeof(T).Name);
-
-            //Get all the properties of that model  
-            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            // Loop through all the properties              
-            // Adding Column name to our datatable  
-            foreach (PropertyInfo prop in Props)
-            {
-                //Setting column names as Property names    
-                dataTable.Columns.Add(prop.Name);
-            }
-            // Adding Row and its value to our dataTable  
-            foreach (T item in models)
-            {
-                var values = new object[Props.Length];
-                for (int i = 0; i < Props.Length; i++)
-                {
-                    //inserting property values to datatable rows    
-                    values[i] = Props[i].GetValue(item, null);
-                }
-                // Finally add value to datatable    
-                dataTable.Rows.Add(values);
-            }
-            return dataTable;
+            var grdReport = new System.Web.UI.WebControls.GridView();
+            grdReport.DataSource = db.Database.SqlQuery<DailyCourierManifesto>("exec uspDailyCourierManiFesto").ToList();
+            grdReport.DataBind();
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            System.Web.UI.HtmlTextWriter htw = new System.Web.UI.HtmlTextWriter(sw);
+            grdReport.RenderControl(htw);
+            byte[] bindData=System.Text.Encoding.ASCII.GetBytes(sw.ToString());
+            return File(bindData, "application/ms-excel","DailyManifesto"+DateTime.Now+".xls");
         }
-
+        
     }
 
     public class UpdateCourrier
