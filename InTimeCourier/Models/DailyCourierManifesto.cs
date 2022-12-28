d222c3133a4b11a355e52547a044bdcbf3ce9a14
Jitendra Dubey <jitendra.dubey@stits.co>
2022-12-28 19:17:53 +0530

    added DailyCourierManifesto class

--- InTimeCourier/Models/DailyCourierManifesto.cs
+++ InTimeCourier/Models/DailyCourierManifesto.cs
@@ -2,5 +2,17 @@
 {
     public class DailyCourierManifesto
     {
+        public string NetworkName { get; set; }
+        public string CourierMode { get; set; }
+        public string PartyName { get; set; }
+        public string CNNo { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string Location { get; set; }
+        public decimal Weight { get; set; }
+        public int Quantity { get; set; }
+        public decimal Rate { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal ODACharges { get; set; }
+        public decimal Amount { get; set; }
     }
 }
